fbf3e5150bd68b310c6f4788bfc27438e2d258b3
sceylan <sezginceyln@gmail.com>
2022-10-03 01:38:55 +0300

    newly created

---
+++ src/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string username;
+    cin>>username;
+    cout<<"we're continue to training";
+    //system("pause");
+    return 0;
+ 
+}
+ 
